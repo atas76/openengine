5c27656c0f6b48edfb4603a5e037d4b833176a6f
Andreas Tasoulas <andreas.tasoulas@gmail.com>
2020-02-29 12:43:09 +0200

    Adjust FGN file contents to language changes

--- src/main/java/org/fgn/parser/StateContext.java
+++ src/main/java/org/fgn/parser/StateContext.java
@@ -6,7 +6,7 @@ public enum StateContext {
     KO("Kick-off"),
     F("Foul"), H("Handball"), SP("Set Piece"), O("Offside"),
     T("Throw In"), C("Corner kick"),
-    GKo("Goal kick over the bar"), GK("Goal kick"),
+    GKoo("Goal kick way over the bar"), GKo("Goal kick over the bar"), GK("Goal kick"),
     HD("Header"), FT("First touch"),
     // GS("Goalkeeper save"),
     G("Goal");
