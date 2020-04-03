ed28c295a977f2407bd9a4172a8aa360c0b353e6
Andreas Tasoulas <andreas.tasoulas@gmail.com>
2020-04-04 00:22:44 +0300

    Add action type enumerator and rename actions

---
+++ src/main/java/org/fgn/ontology/ActionType.java
@@ -0,0 +1,20 @@
+package org.fgn.ontology;
+
+public enum ActionType {
+    High,
+    LowCross,
+    Clearance,
+    BounceOff,
+    HandPass,
+    Move,
+    Pass,
+    Dribble,
+    Forward,
+    Pivot,
+    Cross,
+    Shoot,
+    Hold,
+    LongPass,
+    LongPassBack,
+    SwapWing
+}
