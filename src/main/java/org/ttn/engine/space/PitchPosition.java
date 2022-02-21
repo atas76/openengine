120d036e8f69b4cb0f96580ebcf1473f01c1f038
Andreas Tasoulas <andreas.tasoulas@gmail.com>
2022-02-21 20:12:41 +0200

    Add new pitch position value

--- src/main/java/org/ttn/engine/space/PitchPosition.java
+++ src/main/java/org/ttn/engine/space/PitchPosition.java
@@ -1,5 +1,5 @@
 package org.ttn.engine.space;
 
 public enum PitchPosition {
-    Gkd, Dg, Dp, Dpw, Dwp, Dw, D, Dd, DM, DMw, MDw, Mw, Md, M, MA, AMd, Aw, Awp, A, Apd, Apw, Ap, Ad, KO, CK
+    Gkd, Dg, Dp, Dpw, Dwp, Dw, D, Dd, DM, DMw, MD, MDw, MDd, Mw, Md, M, MA, AMd, Aw, Awp, A, Apd, Apw, Ap, Ad, KO, CK
 }
