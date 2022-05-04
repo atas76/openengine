8074b613bf90c27b7c0acf24b217fa2a67cc172b
Andreas Tasoulas <andreas.tasoulas@gmail.com>
2022-05-04 19:41:07 +0300

    Parse match data from file

--- src/test/java/org/ttn/MatchAnalysisTest.java
+++ src/test/java/org/ttn/MatchAnalysisTest.java
@@ -2,14 +2,23 @@ package org.ttn;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.ttn.lexan.Scanner;
+import org.ttn.lexan.exceptions.ScannerException;
+import org.ttn.parser.Parser;
+import org.ttn.parser.exceptions.ParserException;
+import org.ttn.parser.output.Directive;
+import org.ttn.parser.output.MatchDataElement;
+import org.ttn.parser.output.Statement;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class MatchAnalysisTest {
 
@@ -27,5 +36,20 @@ public class MatchAnalysisTest {
         List<String> matchData = Files.readAllLines(matchSampleResource);
 
         assertEquals(2, matchData.size());
+    }
+
+    @Test
+    public void testParseMatchData() throws IOException {
+        List<MatchDataElement> matchDataElements = Files.lines(matchSampleResource).map(line -> {
+            try {
+                return new Parser().parse(new Scanner(line).scan());
+            } catch (ScannerException | ParserException e) {
+                throw new RuntimeException(e);
+            }
+        }).collect(Collectors.toList());
+
+        assertEquals(2, matchDataElements.size());
+        assertTrue(matchDataElements.get(0) instanceof Directive);
+        assertTrue(matchDataElements.get(1) instanceof Statement);
     }
 }
