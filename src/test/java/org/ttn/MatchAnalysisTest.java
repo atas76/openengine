de54c75d581234a2ede6c8f1f6e2d40e6d4fbcda
Andreas Tasoulas <andreas.tasoulas@gmail.com>
2022-05-25 22:16:30 +0300

    Add data file parsing tests

--- src/test/java/org/ttn/MatchAnalysisTest.java
+++ src/test/java/org/ttn/MatchAnalysisTest.java
@@ -61,7 +61,7 @@ public class MatchAnalysisTest {
         List<MatchDataElement> matchDataElements =
                 new Parser().parse(Files.lines(matchSampleResource).collect(Collectors.toList()));
 
-        assertEquals(834, matchDataElements.size());
+        assertEquals(855, matchDataElements.size());
         assertTrue(matchDataElements.get(0) instanceof Directive); // Kick-off
         assertTrue(matchDataElements.get(1) instanceof Statement); // Default action type
         assertTrue(matchDataElements.get(2) instanceof Directive); // Possession
