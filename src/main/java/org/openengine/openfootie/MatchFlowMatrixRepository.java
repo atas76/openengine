b0d73a22a9c1864f39e936e29e0791e12e6e185b
atas <andreas.tasoulas@gmail.com>
2023-01-30 03:31:07 +0200

    Add match flow matrix v1.0 row: away team kick-off

--- src/main/java/org/openengine/openfootie/MatchFlowMatrixRepository.java
+++ src/main/java/org/openengine/openfootie/MatchFlowMatrixRepository.java
@@ -202,6 +202,10 @@ public class MatchFlowMatrixRepository {
                         new MatchPhaseTransition(PRESSURE, true, 5, 0, 0),
                         new MatchPhaseTransition(PRESSURE, true, 4, 0, 0),
                 }));
+        T_CLF19.addRow(KICK_OFF,
+                new MatchSequence(new MatchPhaseTransition[] {
+                        new MatchPhaseTransition(POSSESSION, true, 4, 0, 0)
+                }));
     }
 
     public static void loadAwayTeamDeprecated() {
