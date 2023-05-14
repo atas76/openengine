20d3084177335dd7cf043ab060685c83b135bad5
atas <andreas.tasoulas@gmail.com>
2023-05-14 05:18:23 +0300

    Add testing utility class for probabilistic testing

---
+++ src/test/java/org/openengine/vanilla/TacticalTestOutput.java
@@ -0,0 +1,43 @@
+package org.openengine.vanilla;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class TacticalTestOutput {
+
+    private final int SAMPLE_SIZE = 100;
+
+    private Map<Position, Integer> actionOutcomes = new HashMap<>();
+    private Map<Team, Integer> matchStates = new HashMap<>();
+
+    public void runTest(Position position) {
+        for (int i = 0; i < SAMPLE_SIZE; i++) {
+            Match match = new Match();
+            match.getState().setPossessionTeam(match.getHomeTeam());
+            match.getState().setPossessionPlayer(match.getHomeTeam().getPlayerByPosition(position));
+            Player player = match.getState().getPossessionPlayer();
+
+            Action action = player.decide();
+            ActionOutcome outcome = match.getState().execute(action);
+            match.updateState(outcome);
+
+            assertEquals(ActionType.Pass, action.getType());
+            actionOutcomes.putIfAbsent(outcome.getPossessionPlayer().getPosition(), 0);
+            actionOutcomes.put(outcome.getPossessionPlayer().getPosition(),
+                    actionOutcomes.get(outcome.getPossessionPlayer().getPosition()) + 1);
+            matchStates.putIfAbsent(match.getState().getPossessionTeam(), 0);
+            matchStates.put(match.getState().getPossessionTeam(),
+                    matchStates.get(match.getState().getPossessionTeam()) + 1);
+        }
+    }
+
+    public double getPossessionOutcomeByPosition(Position position) {
+        return actionOutcomes.get(position) / (double) SAMPLE_SIZE;
+    }
+
+    public double getPossessionOutcomeByTeam(Team team) {
+        return matchStates.get(team) / (double) SAMPLE_SIZE;
+    }
+}
