a7c896632d8f5de7ea1822ec10f81bd644e87c28
atas <andreas.tasoulas@gmail.com>
2023-05-20 22:34:16 +0300

    Add 4-4-2 vs. 4-3-3 probabilistic assertions (centre right midfielder)

--- src/test/java/org/openengine/vanilla/TacticalBehaviour442vs433Test.java
+++ src/test/java/org/openengine/vanilla/TacticalBehaviour442vs433Test.java
@@ -148,4 +148,21 @@ public class TacticalBehaviour442vs433Test {
         assertEquals(0.5 * (1 - xP), testOutput.getPossessionOutcomeByPosition(Position.M_RC), 0.1);
         assertEquals(xP, testOutput.getPossessionOutcomeByTeam(sampleMatch.getHomeTeam()), 0.1);
     }
+
+    @Test
+    public void testCentreRightMidfielderActionsProbabilisticAssertions() {
+        Match sampleMatch = new Match();
+        double xP = sampleMatch.getState().getXP();
+        TacticalTestOutput testOutput = new TacticalTestOutput(Tactics._4_4_2, Tactics._4_3_3);
+
+        testOutput.runTest(Position.M_CR);
+
+        assertEquals(0.33, testOutput.getPossessionOutcomeByPosition(Position.M_R), 0.1);
+        assertEquals(0.33 * xP, testOutput.getPossessionOutcomeByPosition(Position.M_CL), 0.1);
+        assertEquals(0.33 * xP, testOutput.getPossessionOutcomeByPosition(Position.F_CR), 0.1);
+        assertEquals(0.33 * (1 - xP) * 0.5, testOutput.getPossessionOutcomeByPosition(Position.M_C), 0.1);
+        assertEquals(0.33 * (1 - xP) * 0.5, testOutput.getPossessionOutcomeByPosition(Position.M_RC), 0.1);
+        assertEquals(0.33 * (1 - xP), testOutput.getPossessionOutcomeByPosition(Position.D_CL), 0.1);
+        assertEquals(0.33 + 0.33 * xP * 2, testOutput.getPossessionOutcomeByTeam(sampleMatch.getHomeTeam()), 0.1);
+    }
 }
