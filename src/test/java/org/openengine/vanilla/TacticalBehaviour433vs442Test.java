1d3e7e4e25f0cddae91aa9a06c441c1b16ea33c3
atas <andreas.tasoulas@gmail.com>
2023-05-22 20:51:06 +0300

    Add probabilistic assertions (centre right back)

--- src/test/java/org/openengine/vanilla/TacticalBehaviour433vs442Test.java
+++ src/test/java/org/openengine/vanilla/TacticalBehaviour433vs442Test.java
@@ -39,4 +39,21 @@ public class TacticalBehaviour433vs442Test {
         assertEquals(0.33 * (1 - xP) * 0.5 * 1.4, testOutput.getPossessionOutcomeByPosition(Position.M_CR), 0.1);
         assertEquals(0.33 + 0.33 * xP + 0.33 * xP / 1.4, testOutput.getPossessionOutcomeByTeam(sampleMatch.getHomeTeam()), 0.1);
     }
+
+    @Test
+    public void testCentreRightBackActionsProbabilisticAssertions() {
+        Match sampleMatch = new Match();
+        double xP = sampleMatch.getState().getXP();
+        TacticalTestOutput testOutput = new TacticalTestOutput(Tactics._4_3_3, Tactics._4_4_2);
+
+        testOutput.runTest(Position.D_CR);
+
+        assertEquals(0.25, testOutput.getPossessionOutcomeByPosition(Position.GK), 0.1);
+        assertEquals(0.25, testOutput.getPossessionOutcomeByPosition(Position.D_R), 0.1);
+        assertEquals(0.25 * xP, testOutput.getPossessionOutcomeByPosition(Position.D_CL), 0.1);
+        assertEquals(0.25 * (1 - xP), testOutput.getPossessionOutcomeByPosition(Position.F_CR), 0.1);
+        assertEquals(0.25 * xP, testOutput.getPossessionOutcomeByPosition(Position.M_C), 0.1);
+        assertEquals(0.25 * (1 - xP) * 0.5, testOutput.getPossessionOutcomeByPosition(Position.M_CR), 0.1);
+        assertEquals(0.25 * (1 - xP) * 0.5, testOutput.getPossessionOutcomeByPosition(Position.M_CL), 0.1);
+    }
  }
