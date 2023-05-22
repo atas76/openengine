e5ed5dc1d0981344d944c10d4e0d60189950241e
atas <andreas.tasoulas@gmail.com>
2023-05-22 04:10:13 +0300

    Add 4-3-3 vs. 4-4-2 probabilistic assertions (left back)

--- src/test/java/org/openengine/vanilla/TacticalBehaviour433vs442Test.java
+++ src/test/java/org/openengine/vanilla/TacticalBehaviour433vs442Test.java
@@ -21,4 +21,20 @@ public class TacticalBehaviour433vs442Test {
         assertEquals(0.33 * (1 - xP) * 0.5 * 1.4, testOutput.getPossessionOutcomeByPosition(Position.M_L), 0.1);
         assertEquals(0.33 * (1 - xP) * 0.5 * 1.4, testOutput.getPossessionOutcomeByPosition(Position.M_CL), 0.1);
     }
-}
+
+    @Test
+    public void testLeftBackActionsProbabilisticAssertions() {
+        Match sampleMatch = new Match();
+        double xP = sampleMatch.getState().getXP();
+        TacticalTestOutput testOutput = new TacticalTestOutput(Tactics._4_3_3, Tactics._4_4_2);
+
+        testOutput.runTest(Position.D_L);
+
+        assertEquals(0.33, testOutput.getPossessionOutcomeByPosition(Position.GK), 0.1);
+        assertEquals(0.33 * xP, testOutput.getPossessionOutcomeByPosition(Position.D_CL), 0.1);
+        assertEquals(0.33 * xP / 1.4, testOutput.getPossessionOutcomeByPosition(Position.M_LC), 0.1);
+        assertEquals(0.33 * (1 - xP), testOutput.getPossessionOutcomeByPosition(Position.F_CR), 0.1);
+        assertEquals(0.33 * (1 - xP) * 0.5 * 1.4, testOutput.getPossessionOutcomeByPosition(Position.M_R), 0.1);
+        assertEquals(0.33 * (1 - xP) * 0.5 * 1.4, testOutput.getPossessionOutcomeByPosition(Position.M_CR), 0.1);
+    }
+ }
