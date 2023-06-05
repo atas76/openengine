a1931e77af9daac3a96415badf278d043d7e36d6
atas <andreas.tasoulas@gmail.com>
2023-06-06 01:26:16 +0300

    Refactor goalkeeper action tests; corrections

--- src/test/java/org/openengine/vanilla/TacticalBehaviour433GlobalPassingTest.java
+++ src/test/java/org/openengine/vanilla/TacticalBehaviour433GlobalPassingTest.java
@@ -4,9 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openengine.vanilla.util.Flags;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.junit.Assert.assertEquals;
 
 public class TacticalBehaviour433GlobalPassingTest {
@@ -14,7 +11,6 @@ public class TacticalBehaviour433GlobalPassingTest {
     private final double xP = 0.7;
     private TacticalTestOutput testOutput;
     private final double DELTA = 0.15;
-    private final static int SAMPLE_SIZE = 100;
 
     @Before
     public void setUp() {
@@ -24,39 +20,17 @@ public class TacticalBehaviour433GlobalPassingTest {
 
     @Test
     public void testGoalkeeperActionsProbabilisticAssertions() {
+        testOutput.runTest(Position.GK);
 
-        Map<Position, Integer> actionOutcomes = new HashMap<>();
-        Map<Team, Integer> matchStates = new HashMap<>();
-
-        for (int i = 0; i < SAMPLE_SIZE; i++) {
-            Match match = new Match(Tactics._4_3_3, Tactics._4_3_3);
-            match.getState().setXP(xP);
-            match.getState().setPossessionTeam(match.getHomeTeam());
-            match.getState().setPossessionPlayer(match.getHomeTeam().getGoalkeeper());
-            Player goalkeeper = match.getState().getPossessionPlayer();
-
-            Action action = goalkeeper.decide();
-            ActionOutcome outcome = match.getState().execute(action);
-            match.updateState(outcome);
-
-            assertEquals(ActionType.Pass, action.getType());
-            actionOutcomes.putIfAbsent(outcome.getPossessionPlayer().getPosition(), 0);
-            actionOutcomes.put(outcome.getPossessionPlayer().getPosition(),
-                    actionOutcomes.get(outcome.getPossessionPlayer().getPosition()) + 1);
-            matchStates.putIfAbsent(match.getState().getPossessionTeam(), 0);
-            matchStates.put(match.getState().getPossessionTeam(),
-                    matchStates.get(match.getState().getPossessionTeam()) + 1);
-        }
-
-        assertEquals(0.1 * xP * 0.5, actionOutcomes.get(Position.D_R) / (double) SAMPLE_SIZE, DELTA);
-        assertEquals(0.1 * xP * 0.5, actionOutcomes.get(Position.D_L) / (double) SAMPLE_SIZE, DELTA);
-        assertEquals(0.1 * xP, actionOutcomes.get(Position.D_CR) / (double) SAMPLE_SIZE, DELTA);
-        assertEquals(0.1 * xP, actionOutcomes.get(Position.D_CL) / (double) SAMPLE_SIZE, DELTA);
-        assertEquals(0.05 * xP, actionOutcomes.get(Position.M_RC) / (double) SAMPLE_SIZE, DELTA);
-        assertEquals(0.05 * xP, actionOutcomes.get(Position.M_C) / (double) SAMPLE_SIZE, DELTA);
-        assertEquals(0.05 * xP, actionOutcomes.get(Position.M_LC) / (double) SAMPLE_SIZE, DELTA);
-        assertEquals(0.033 * xP, actionOutcomes.get(Position.F_RC) / (double) SAMPLE_SIZE, DELTA);
-        assertEquals(0.033 * xP, actionOutcomes.get(Position.F_C) / (double) SAMPLE_SIZE, DELTA);
-        assertEquals(0.033 * xP, actionOutcomes.get(Position.F_LC) / (double) SAMPLE_SIZE, DELTA);
+        assertEquals(0.1 * xP * 2.0, testOutput.getPossessionOutcomeByPosition(Position.D_R), DELTA);
+        assertEquals(0.1 * xP * 2.0, testOutput.getPossessionOutcomeByPosition(Position.D_L), DELTA);
+        assertEquals(0.1 * xP, testOutput.getPossessionOutcomeByPosition(Position.D_CR), DELTA);
+        assertEquals(0.1 * xP, testOutput.getPossessionOutcomeByPosition(Position.D_CL), DELTA);
+        assertEquals(0.05 * xP, testOutput.getPossessionOutcomeByPosition(Position.M_RC), DELTA);
+        assertEquals(0.05 * xP, testOutput.getPossessionOutcomeByPosition(Position.M_C), DELTA);
+        assertEquals(0.05 * xP, testOutput.getPossessionOutcomeByPosition(Position.M_LC), DELTA);
+        assertEquals(0.033 * xP, testOutput.getPossessionOutcomeByPosition(Position.F_RC), DELTA);
+        assertEquals(0.033 * xP, testOutput.getPossessionOutcomeByPosition(Position.F_C), DELTA);
+        assertEquals(0.033 * xP, testOutput.getPossessionOutcomeByPosition(Position.F_LC), DELTA);
     }
 }
--- src/test/java/org/openengine/vanilla/TacticalBehaviour442GlobalPassingTest.java
+++ src/test/java/org/openengine/vanilla/TacticalBehaviour442GlobalPassingTest.java
@@ -4,9 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openengine.vanilla.util.Flags;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.junit.Assert.assertEquals;
 
 public class TacticalBehaviour442GlobalPassingTest {
@@ -14,7 +11,6 @@ public class TacticalBehaviour442GlobalPassingTest {
     private final double xP = 0.7;
     private TacticalTestOutput testOutput;
     private final double DELTA = 0.15;
-    private final static int SAMPLE_SIZE = 100;
 
     @Before
     public void setUp() {
@@ -24,39 +20,18 @@ public class TacticalBehaviour442GlobalPassingTest {
 
     @Test
     public void testGoalkeeperActionsProbabilisticAssertions() {
+        testOutput.runTest(Position.GK);
 
-        Map<Position, Integer> actionOutcomes = new HashMap<>();
-        Map<Team, Integer> matchStates = new HashMap<>();
-
-        for (int i = 0; i < SAMPLE_SIZE; i++) {
-            Match match = new Match();
-            match.getState().setPossessionTeam(match.getHomeTeam());
-            match.getState().setPossessionPlayer(match.getHomeTeam().getGoalkeeper());
-            Player goalkeeper = match.getState().getPossessionPlayer();
-
-            Action action = goalkeeper.decide();
-            ActionOutcome outcome = match.getState().execute(action);
-            match.updateState(outcome);
-
-            assertEquals(ActionType.Pass, action.getType());
-            actionOutcomes.putIfAbsent(outcome.getPossessionPlayer().getPosition(), 0);
-            actionOutcomes.put(outcome.getPossessionPlayer().getPosition(),
-                    actionOutcomes.get(outcome.getPossessionPlayer().getPosition()) + 1);
-            matchStates.putIfAbsent(match.getState().getPossessionTeam(), 0);
-            matchStates.put(match.getState().getPossessionTeam(),
-                    matchStates.get(match.getState().getPossessionTeam()) + 1);
-        }
-
-        assertEquals(0.1, actionOutcomes.get(Position.D_R) / (double) SAMPLE_SIZE, DELTA);
-        assertEquals(0.1, actionOutcomes.get(Position.D_L) / (double) SAMPLE_SIZE, DELTA);
-        assertEquals(0.1 * xP, actionOutcomes.get(Position.D_CR) / (double) SAMPLE_SIZE, DELTA);
-        assertEquals(0.1 * xP, actionOutcomes.get(Position.D_CL) / (double) SAMPLE_SIZE, DELTA);
-        assertEquals(0.05 * xP, actionOutcomes.get(Position.M_R) / (double) SAMPLE_SIZE, DELTA);
-        assertEquals(0.05 * xP, actionOutcomes.get(Position.M_CR) / (double) SAMPLE_SIZE, DELTA);
-        assertEquals(0.05 * xP, actionOutcomes.get(Position.M_CL) / (double) SAMPLE_SIZE, DELTA);
-        assertEquals(0.05 * xP, actionOutcomes.get(Position.M_L) / (double) SAMPLE_SIZE, DELTA);
-        assertEquals(0.033 * xP, actionOutcomes.get(Position.F_CR) / (double) SAMPLE_SIZE, DELTA);
-        assertEquals(0.033 * xP, actionOutcomes.get(Position.F_CL) / (double) SAMPLE_SIZE, DELTA);
+        assertEquals(0.1, testOutput.getPossessionOutcomeByPosition(Position.D_R), DELTA);
+        assertEquals(0.1, testOutput.getPossessionOutcomeByPosition(Position.D_L), DELTA);
+        assertEquals(0.1 * xP, testOutput.getPossessionOutcomeByPosition(Position.D_CR), DELTA);
+        assertEquals(0.1 * xP, testOutput.getPossessionOutcomeByPosition(Position.D_CL), DELTA);
+        assertEquals(0.05 * xP, testOutput.getPossessionOutcomeByPosition(Position.M_R), DELTA);
+        assertEquals(0.05 * xP, testOutput.getPossessionOutcomeByPosition(Position.M_CR), DELTA);
+        assertEquals(0.05 * xP, testOutput.getPossessionOutcomeByPosition(Position.M_CL), DELTA);
+        assertEquals(0.05 * xP, testOutput.getPossessionOutcomeByPosition(Position.M_L), DELTA);
+        assertEquals(0.033 * xP, testOutput.getPossessionOutcomeByPosition(Position.F_CR), DELTA);
+        assertEquals(0.033 * xP, testOutput.getPossessionOutcomeByPosition(Position.F_CL), DELTA);
     }
 
     @Test
