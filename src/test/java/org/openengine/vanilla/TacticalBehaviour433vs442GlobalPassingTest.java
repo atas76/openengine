2f3cfac91dbcf0a2783054570bbbd706655926c7
atas <andreas.tasoulas@gmail.com>
2023-08-04 00:07:22 +0300

    Add 4-3-3 vs. 4-4-2 test (centre left defender)

--- src/test/java/org/openengine/vanilla/TacticalBehaviour433vs442GlobalPassingTest.java
+++ src/test/java/org/openengine/vanilla/TacticalBehaviour433vs442GlobalPassingTest.java
@@ -84,4 +84,21 @@ public class TacticalBehaviour433vs442GlobalPassingTest {
         assertEquals(0.14 * xP / (2 * VERTICAL_DISTANCE_UNIT_FACTOR),
                 testOutput.getPossessionOutcomeByPosition(Position.F_C), DELTA);
     }
+
+    @Test
+    public void testCentreLeftDefenderActionsProbabilisticAssertions() {
+        testOutput.runTest(Position.D_CL);
+
+        assertEquals(0.14, testOutput.getPossessionOutcomeByPosition(Position.GK), DELTA);
+        assertEquals(0.14, testOutput.getPossessionOutcomeByPosition(Position.D_L), DELTA);
+        assertEquals(0.14 * xP, testOutput.getPossessionOutcomeByPosition(Position.D_CR), DELTA);
+        assertEquals(0.14 * xP / VERTICAL_DISTANCE_UNIT_FACTOR,
+                testOutput.getPossessionOutcomeByPosition(Position.M_C), DELTA);
+        assertEquals(0.14 * xP / VERTICAL_DISTANCE_UNIT_FACTOR,
+                testOutput.getPossessionOutcomeByPosition(Position.M_LC), DELTA);
+        assertEquals(0.14 * xP / (2 * VERTICAL_DISTANCE_UNIT_FACTOR),
+                testOutput.getPossessionOutcomeByPosition(Position.F_LC), DELTA);
+        assertEquals(0.14 * xP / (2 * VERTICAL_DISTANCE_UNIT_FACTOR),
+                testOutput.getPossessionOutcomeByPosition(Position.F_C), DELTA);
+    }
 }
