09323c67517fd91ccce5dfada8b829b2cbd90480
atas <andreas.tasoulas@gmail.com>
2023-09-23 03:13:25 +0300

    Refactor: remove separate lexical analyzer object

--- src/main/java/org/mpn/Parser.java
+++ src/main/java/org/mpn/Parser.java
@@ -8,8 +8,22 @@ public class Parser {
     private List<String> tokens = new ArrayList<>();
 
     public Statement parse(String line) {
-        tokens = new Lexan().getTokens(line);
+        tokens = getTokens(line);
         return new Statement(getTeamKey(), -1, -1, null, null);
+    }
+
+    private List<String> getTokens(String expression) {
+
+        List<String> tokens = new ArrayList<>();
+        int index = 0;
+        if (Character.isAlphabetic(expression.charAt(0))) {
+            while (Character.isAlphabetic(expression.charAt(index))) {
+                ++index;
+            }
+            tokens.add(expression.substring(0, index));
+        }
+        if (index > 0 && index < expression.length()) tokens.addAll(getTokens(expression.substring(index)));
+        return tokens;
     }
 
     private String getTeamKey() {
