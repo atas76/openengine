92cdd27c632b7aa3042e7da9a4f033687b1cbbf2
atas <andreas.tasoulas@gmail.com>
2023-10-13 19:53:28 +0300

    Add PENALTY enum value

--- src/main/java/org/mpn/State.java
+++ src/main/java/org/mpn/State.java
@@ -1,12 +1,13 @@
 package org.mpn;
 
 public enum State {
-    KICK_OFF, ATTACK;
+    KICK_OFF, ATTACK, PENALTY;
 
     public static State createFromName(String name) {
         return switch (name) {
             case "KickOff" -> KICK_OFF;
             case "Attack" -> ATTACK;
+            case "Penalty" -> PENALTY;
             default -> null;
         };
     }
