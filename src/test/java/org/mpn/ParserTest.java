d44817fe36a02c7acc252d6f156a534f3ff0dd95
atas <andreas.tasoulas@gmail.com>
2023-10-13 19:54:11 +0300

    Add test for statement time span definition

--- src/test/java/org/mpn/ParserTest.java
+++ src/test/java/org/mpn/ParserTest.java
@@ -17,15 +17,24 @@ public class ParserTest {
         assertEquals("L", statement.getTeamKey());
         assertEquals(0, statement.getMinutes());
         assertEquals(0, statement.getSeconds());
+        assertEquals(0, statement.getEndMinutes());
+        assertEquals(0, statement.getEndSeconds());
         assertEquals(State.KICK_OFF, statement.getInitialState());
         assertEquals(State.ATTACK, statement.getEndState());
     }
 
+    @Test
     public void testTimeSpan() throws Exception {
         Parser parser = new Parser();
 
         Statement statement = parser.parse("L: 00:19 => 00:21 Attack -> Penalty");
-
+        assertEquals("L", statement.getTeamKey());
+        assertEquals(0, statement.getMinutes());
+        assertEquals(19, statement.getSeconds());
+        assertEquals(0, statement.getEndMinutes());
+        assertEquals(21, statement.getEndSeconds());
+        assertEquals(State.ATTACK, statement.getInitialState());
+        assertEquals(State.PENALTY, statement.getEndState());
     }
 
     @Test
