59abea69fba0dec3cf030af68b5b0388ffff2db1
atas <andreas.tasoulas@gmail.com>
2023-10-15 02:17:07 +0300

    Add new states: Corner, Goal

--- src/main/java/org/mpn/State.java
+++ src/main/java/org/mpn/State.java
@@ -1,13 +1,15 @@
 package org.mpn;
 
 public enum State {
-    KICK_OFF, ATTACK, PENALTY;
+    KICK_OFF, ATTACK, PENALTY, GOAL, CORNER;
 
     public static State createFromName(String name) {
         return switch (name) {
             case "KickOff" -> KICK_OFF;
             case "Attack" -> ATTACK;
             case "Penalty" -> PENALTY;
+            case "Goal" -> GOAL;
+            case "Corner" -> CORNER;
             default -> null;
         };
     }
