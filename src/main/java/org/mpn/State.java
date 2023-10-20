fc76ec13ff74a549260b6770430c96d76d8d3ef4
atas <andreas.tasoulas@gmail.com>
2023-10-21 01:33:50 +0300

    Add new states: ThrowIn, Transition

--- src/main/java/org/mpn/State.java
+++ src/main/java/org/mpn/State.java
@@ -1,7 +1,7 @@
 package org.mpn;
 
 public enum State {
-    KICK_OFF, ATTACK, PENALTY, GOAL, CORNER;
+    KICK_OFF, ATTACK, PENALTY, GOAL, CORNER, THROW_IN, TRANSITION;
 
     public static State createFromName(String name) {
         return switch (name) {
@@ -10,6 +10,8 @@ public enum State {
             case "Penalty" -> PENALTY;
             case "Goal" -> GOAL;
             case "Corner" -> CORNER;
+            case "ThrowIn" -> THROW_IN;
+            case "Transition" -> TRANSITION;
             default -> null;
         };
     }
