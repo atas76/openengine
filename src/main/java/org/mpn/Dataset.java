29e0ccf0e8d42e3e14b72f7fc51a03d9eb5dcd5c
atas <andreas.tasoulas@gmail.com>
2024-01-19 04:51:32 +0200

    Filtering by process unit type; filtering directives

--- src/main/java/org/mpn/Dataset.java
+++ src/main/java/org/mpn/Dataset.java
@@ -1,6 +1,8 @@
 package org.mpn;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Dataset {
 
@@ -10,7 +12,38 @@ public class Dataset {
         this.data = data;
     }
 
+    public List<Directive> getDirectives() {
+        return getByProcessUnitType(Directive.class).stream()
+                .map(Directive.class::cast)
+                .toList();
+
+    }
+
+    public long countDirectives(Directive directive) {
+        return getDirectives().stream()
+                .filter(d -> d.equals(directive))
+                .count();
+    }
+
+    public List<Integer> getDirectiveLocations(Directive directive) {
+        return IntStream.range(0, data.size())
+                .filter(index -> data.get(index) instanceof Directive && data.get(index).equals(directive))
+                .boxed()
+                .collect(Collectors.toList());
+
+    }
+
+    public List<Statement> getStatements() {
+        return getByProcessUnitType(Statement.class).stream()
+                .map(Statement.class::cast)
+                .toList();
+    }
+
     public int size() {
         return data.size();
+    }
+
+    private List<? extends ProcessUnit> getByProcessUnitType(Class<? extends ProcessUnit> clazz) {
+        return data.stream().filter(clazz::isInstance).toList();
     }
 }
--- src/main/java/org/mpn/ProcessorClient.java
+++ src/main/java/org/mpn/ProcessorClient.java
@@ -13,5 +13,12 @@ public class ProcessorClient {
         Dataset dataset = new Dataset(data);
 
         System.out.println("Raw dataset size: " + dataset.size());
+        System.out.println("Number of directives: " + dataset.getDirectives().size());
+        System.out.println("Number of BREAK directives: " + dataset.countDirectives(Directive.BREAK));
+        System.out.println("Number of half time directives: " + dataset.countDirectives(Directive.HT));
+        System.out.println("BREAK directive locations: " + dataset.getDirectiveLocations(Directive.BREAK));
+        System.out.println("Half time directive location: " + dataset.getDirectiveLocations(Directive.HT));
+        System.out.println("Number of statements: " + dataset.getStatements().size());
+
     }
 }
