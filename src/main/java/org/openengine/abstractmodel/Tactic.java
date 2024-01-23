aaec19235768c36e7bc099048ac82e0d9da7db60
atas <andreas.tasoulas@gmail.com>
2024-01-23 05:24:16 +0200

    Load tactics

--- src/main/java/org/openengine/abstractmodel/Tactic.java
+++ src/main/java/org/openengine/abstractmodel/Tactic.java
@@ -1,13 +1,29 @@
 package org.openengine.abstractmodel;
 
+import java.util.Arrays;
+
 public class Tactic {
 
     private final TacticalPosition [] positions = new TacticalPosition[10];
-    private Double [][] weightLayout = new Double[5][7];
+    private double [][] weightLayout = new double[5][7];
 
     public Tactic(TacticalPosition [] positions) {
         for (int i = 0; i < 10; i++) {
             this.positions[i] = positions[i];
+            weightLayout[positions[i].getX()][positions[i].getY()] = 1.0;
         }
+    }
+
+    @Override
+    public String toString() {
+         StringBuilder sb = new StringBuilder();
+         for (int i = 0; i < weightLayout.length; i++) {
+           for (int j = 0; j < weightLayout[i].length; j++) {
+               sb.append(String.format("%.2f", weightLayout[i][j]));
+               sb.append(" ");
+           }
+           sb.append("\n");
+        }
+        return sb.toString();
     }
 }
---
+++ src/main/java/org/openengine/abstractmodel/TacticsClient.java
@@ -0,0 +1,22 @@
+package org.openengine.abstractmodel;
+
+public class TacticsClient {
+    public static void main(String[] args) {
+        Tactic tactic_4_4_2 = new Tactic(Formations.formation_4_4_2);
+        Tactic tactic_4_3_3 = new Tactic(Formations.formation_4_3_3);
+        Tactic tactic_4_2_3_1 = new Tactic(Formations.formation_4_2_3_1);
+
+        System.out.println("4-4-2");
+        System.out.println(tactic_4_4_2);
+
+        System.out.println();
+
+        System.out.println("4-3-3");
+        System.out.println(tactic_4_3_3);
+
+        System.out.println();
+
+        System.out.println("4-2-3-1");
+        System.out.println(tactic_4_2_3_1);
+    }
+}
