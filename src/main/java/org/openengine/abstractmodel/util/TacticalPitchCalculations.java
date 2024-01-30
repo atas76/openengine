3745257853fc08f3e1ae80253982284fe5a007fa
atas <andreas.tasoulas@gmail.com>
2024-01-30 03:42:10 +0200

    Add utility method for matrix addition

--- src/main/java/org/openengine/abstractmodel/util/TacticalPitchCalculations.java
+++ src/main/java/org/openengine/abstractmodel/util/TacticalPitchCalculations.java
@@ -5,4 +5,17 @@ public class TacticalPitchCalculations {
     public static int getDistance(Point a, Point b) {
         return (int) Math.round(Math.sqrt(Math.pow(a.x() - b.x(), 2) + Math.pow(a.y() - b.y(), 2)));
     }
+
+    public static double [][] add(double[][] a, double[][] b, int width, int height) {
+
+        double [][] result = new double[width][height];
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                result[i][j] = a[i][j] + b[i][j];
+            }
+        }
+
+        return result;
+    }
 }
--- src/test/java/org/openengine/abstractmodel/util/TacticalPitchCalculationsTest.java
+++ src/test/java/org/openengine/abstractmodel/util/TacticalPitchCalculationsTest.java
@@ -2,6 +2,7 @@ package org.openengine.abstractmodel.util;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class TacticalPitchCalculationsTest {
@@ -18,5 +19,36 @@ public class TacticalPitchCalculationsTest {
         assertEquals(4, TacticalPitchCalculations.getDistance(a1, b1));
         assertEquals(7, TacticalPitchCalculations.getDistance(a2, b2));
         assertEquals(4, TacticalPitchCalculations.getDistance(a3, b3));
+    }
+
+    @Test
+    public void testMatrixAddition() {
+        double[][] matrix1 = {
+                {0.12, 0.15, 0.18, 0.20, 0.18, 0.15, 0.12},
+                {0.15, 0.25, 0.30, 0.35, 0.30, 0.25, 0.15},
+                {0.18, 0.30, 0.45, 0.50, 0.45, 0.30, 0.18},
+                {0.15, 0.25, 0.30, 0.35, 0.30, 0.25, 0.15},
+                {0.12, 0.15, 0.18, 0.20, 0.18, 0.15, 0.12}
+        };
+
+        double[][] matrix2 = {
+                {0.10, 0.12, 0.14, 0.15, 0.14, 0.12, 0.10},
+                {0.12, 0.18, 0.25, 0.28, 0.25, 0.18, 0.12},
+                {0.14, 0.25, 0.30, 0.40, 0.30, 0.25, 0.14},
+                {0.12, 0.18, 0.25, 0.28, 0.25, 0.18, 0.12},
+                {0.10, 0.12, 0.14, 0.15, 0.14, 0.12, 0.10}
+        };
+
+        double[][] expectedSum = {
+                {0.22, 0.27, 0.32, 0.35, 0.32, 0.27, 0.22},
+                {0.27, 0.43, 0.55, 0.63, 0.55, 0.43, 0.27},
+                {0.32, 0.55, 0.75, 0.90, 0.75, 0.55, 0.32},
+                {0.27, 0.43, 0.55, 0.63, 0.55, 0.43, 0.27},
+                {0.22, 0.27, 0.32, 0.35, 0.32, 0.27, 0.22}
+        };
+
+        double[][] result = TacticalPitchCalculations.add(matrix1, matrix2, 5, 7);
+
+        assertArrayEquals(expectedSum, result);
     }
 }
