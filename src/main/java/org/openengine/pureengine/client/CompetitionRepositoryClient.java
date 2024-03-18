5bae78520931b6026951cbc6f12c39f31f0c4b0f
atas <andreas.tasoulas@gmail.com>
2024-03-18 21:27:35 +0200

    Renaming

--- src/main/java/org/openengine/pureengine/client/CompetitionRepositoryClient.java
+++ src/main/java/org/openengine/pureengine/client/CompetitionRepositoryClient.java
@@ -3,7 +3,7 @@ package org.openengine.pureengine.client;
 import org.openengine.pureengine.tournament.Competition;
 import org.openengine.pureengine.tournament.CompetitionRepository;
 
-public class CompetitionClient {
+public class CompetitionRepositoryClient {
 
     public static void main(String[] args) {
         Competition competition = CompetitionRepository.ENGLISH_FA_CUP;
