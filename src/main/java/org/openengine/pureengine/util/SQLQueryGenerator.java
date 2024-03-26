ef1fc0ca2ec9cda62c541a4f8d0492d18e08aba0
atas <andreas.tasoulas@gmail.com>
2024-03-27 00:06:22 +0200

    Generate database team 'insert' statements

---
+++ src/main/java/org/openengine/pureengine/util/SQLQueryGenerator.java
@@ -0,0 +1,47 @@
+package org.openengine.pureengine.util;
+
+import org.openengine.pureengine.domain.CommonUtil;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SQLQueryGenerator {
+
+    public static void main(String[] args) {
+        teamInitStatements(CommonUtil.DOMAIN_ROOT + "/team.csv").forEach(System.out::println);
+    }
+
+    public static List<String> teamInitStatements(String datasource) {
+
+        List<String> sqlStatements = new ArrayList<>();
+
+        try (var records = Files.lines(Paths.get(datasource))) {
+            records.forEach(record -> {
+                try {
+                    if (!record.startsWith("#")) {
+                        String [] csvRecord = CommonUtil.parseCsv(record);
+                        String query = "INSERT INTO Team(team_name, full_name, skill) " +
+                                "VALUES(" +
+                                "'" + csvRecord[0] + "'" +
+                                "," +
+                                "'" + csvRecord[1] + "'" +
+                                "," +
+                                csvRecord[2] +
+                                ");";
+                        sqlStatements.add(query);
+                    }
+                } catch (Exception e) {
+                    System.out.println(record);
+                    throw new RuntimeException(e);
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return sqlStatements;
+    }
+}
