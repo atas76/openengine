9b765f80715eefc74a777b1452334207994fa709
atas <andreas.tasoulas@gmail.com>
2024-03-27 16:12:35 +0200

    Generate 'insert' statements for Competition database table

--- src/main/java/org/openengine/pureengine/util/SQLQueryGenerator.java
+++ src/main/java/org/openengine/pureengine/util/SQLQueryGenerator.java
@@ -11,7 +11,41 @@ import java.util.List;
 public class SQLQueryGenerator {
 
     public static void main(String[] args) {
-        teamInitStatements(CommonUtil.DOMAIN_ROOT + "/team.csv").forEach(System.out::println);
+        // teamInitStatements(CommonUtil.DOMAIN_ROOT + "/team.csv").forEach(System.out::println);
+        competitionInitStatements(CommonUtil.DOMAIN_ROOT + "/competition.csv").forEach(System.out::println);
+    }
+
+    public static List<String> competitionInitStatements(String datasource) {
+
+        List<String> sqlStatements = new ArrayList<>();
+
+        try (var records = Files.lines(Paths.get(datasource))) {
+            records.forEach(record -> {
+                try {
+                    if (!record.startsWith("#")) {
+                        String [] csvRecord = CommonUtil.parseCsv(record);
+                        String query = "INSERT INTO Competition(id, name, country, country_demonym) " +
+                                "VALUES("
+                                + csvRecord[0] +
+                                "," +
+                                "'" + csvRecord[1] + "'" +
+                                "," +
+                                "'" + csvRecord[2] + "'" +
+                                "," +
+                                "'" + csvRecord[3] + "'" +
+                                ");";
+                        sqlStatements.add(query);
+                    }
+                } catch (Exception e) {
+                    System.out.println(record);
+                    throw new RuntimeException(e);
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return sqlStatements;
     }
 
     public static List<String> teamInitStatements(String datasource) {
