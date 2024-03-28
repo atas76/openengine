41cf7412516d03b7a4bf8aad9640e1f8f4acb6fe
atas <andreas.tasoulas@gmail.com>
2024-03-28 13:34:09 +0200

    Generate 'insert' statements for CompetitionRound database table

--- src/main/java/org/openengine/pureengine/util/SQLQueryGenerator.java
+++ src/main/java/org/openengine/pureengine/util/SQLQueryGenerator.java
@@ -12,7 +12,41 @@ public class SQLQueryGenerator {
 
     public static void main(String[] args) {
         // teamInitStatements(CommonUtil.DOMAIN_ROOT + "/team.csv").forEach(System.out::println);
-        competitionInitStatements(CommonUtil.DOMAIN_ROOT + "/competition.csv").forEach(System.out::println);
+        // competitionInitStatements(CommonUtil.DOMAIN_ROOT + "/competition.csv").forEach(System.out::println);
+        competitionRoundInitStatements(CommonUtil.DOMAIN_ROOT + "/competition_round.csv").forEach(System.out::println);
+    }
+
+    public static List<String> competitionRoundInitStatements(String datasource) {
+
+        List<String> sqlStatements = new ArrayList<>();
+
+        try (var records = Files.lines(Paths.get(datasource))) {
+            records.forEach(record -> {
+                try {
+                    if (!record.startsWith("#")) {
+                        String [] csvRecord = CommonUtil.parseCsv(record);
+                        String query = "INSERT INTO CompetitionRound(name, home_advantage, tie_breaker, competition_id) "
+                                + "VALUES("
+                                + "'" + csvRecord[0] + "'"
+                                + ","
+                                + csvRecord[1]
+                                + ","
+                                + "'" + csvRecord[2] + "'"
+                                + ","
+                                + csvRecord[3]
+                                + ");";
+                        sqlStatements.add(query);
+                    }
+                } catch (Exception e) {
+                    System.out.println(record);
+                    throw new RuntimeException(e);
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return sqlStatements;
     }
 
     public static List<String> competitionInitStatements(String datasource) {
