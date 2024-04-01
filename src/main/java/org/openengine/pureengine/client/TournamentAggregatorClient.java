d22fc3dc10b5f6fac3b14e6e64debe0cef2f895b
atas <andreas.tasoulas@gmail.com>
2024-04-01 16:39:10 +0300

    Display aggregator statistics for both supported seasons

--- src/main/java/org/openengine/pureengine/client/TournamentAggregatorClient.java
+++ src/main/java/org/openengine/pureengine/client/TournamentAggregatorClient.java
@@ -6,8 +6,18 @@ import org.openengine.pureengine.domain.repository.TournamentRepository;
 public class TournamentAggregatorClient {
 
     public static void main(String[] args) {
-        TournamentAggregator tournamentAggregator = new TournamentAggregator(TournamentRepository.ENGLISH_FA_CUP_2023);
-        tournamentAggregator.execute();
-        tournamentAggregator.displayResults();
+        System.out.println("English FA Cup 2022-23");
+        System.out.println();
+        TournamentAggregator tournamentAggregator23 = new TournamentAggregator(TournamentRepository.ENGLISH_FA_CUP_2023);
+        tournamentAggregator23.execute();
+        tournamentAggregator23.displayResults();
+
+        System.out.println();
+
+        System.out.println("English FA Cup 2023-24");
+        System.out.println();
+        TournamentAggregator tournamentAggregator24 = new TournamentAggregator(TournamentRepository.ENGLISH_FA_CUP_2024);
+        tournamentAggregator24.execute();
+        tournamentAggregator24.displayResults();
     }
 }
