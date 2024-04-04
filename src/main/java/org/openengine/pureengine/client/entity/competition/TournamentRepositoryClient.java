90c1da9b45951a7238f8a0473fd76411bf0a5bf8
atas <andreas.tasoulas@gmail.com>
2024-04-04 16:05:25 +0300

    Add client for tournament repository demo
    This is the starting point of the tournaments simulation application, and as it is going to be replaced with one with actual database support, it is copied to be together with the rest of 'repository client' demo classes.

---
+++ src/main/java/org/openengine/pureengine/client/entity/competition/TournamentRepositoryClient.java
@@ -0,0 +1,25 @@
+package org.openengine.pureengine.client.entity.competition;
+
+import org.openengine.pureengine.domain.model.Tournament;
+import org.openengine.pureengine.domain.repository.TournamentRepository;
+
+import java.util.List;
+
+public class TournamentRepositoryClient {
+    public static void main(String[] args) {
+
+        List<Tournament> tournamentHistory = List.of(
+                TournamentRepository.ENGLISH_FA_CUP_2023,
+                TournamentRepository.ENGLISH_FA_CUP_2024);
+
+        tournamentHistory.forEach(tournament -> {
+            tournament.displayHeader();
+            System.out.println();
+            tournament.displayParticipants();
+
+            System.out.println();
+            tournament.play();
+            System.out.println();
+        });
+    }
+}
