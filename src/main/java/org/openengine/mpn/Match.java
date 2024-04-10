19f2d4b4a8b2bf2ea3d5892d8e20df493f9e98d7
atas <andreas.tasoulas@gmail.com>
2024-04-10 14:09:22 +0300

    Minor

--- src/main/java/org/openengine/mpn/Match.java
+++ src/main/java/org/openengine/mpn/Match.java
@@ -137,7 +137,7 @@ public class Match {
                     case THROW_IN -> {
                         if (statement.isPossessionRetained()) {
                             commentary.append(teamName);
-                            commentary.append(" win a throw-in from position ");
+                            commentary.append(" win a throw-in in pitch position ");
                             commentary.append(outcomePitchPosition);
                             attachPitchPositionDescription(outcomePitchPosition, commentary);
                         } else {
