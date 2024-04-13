9783773e49f9bbead121f5ff0e7710201678d7cc
atas <andreas.tasoulas@gmail.com>
2024-04-13 14:43:17 +0300

    Remove unused import

--- src/main/java/org/openengine/pureengine/domain/repository/LoadableRepository.java
+++ src/main/java/org/openengine/pureengine/domain/repository/LoadableRepository.java
@@ -1,7 +1,5 @@
 package org.openengine.pureengine.domain.repository;
 
-import org.openengine.pureengine.domain.repository.Repository;
-
 public interface LoadableRepository<T> extends Repository<T> {
     void loadData();
 }
