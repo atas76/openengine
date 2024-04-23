ff68a3bb9032cfdc0c08ec65b33ef52b8ce0445c
atas <andreas.tasoulas@gmail.com>
2024-04-23 23:19:55 +0300

    Grammatical match commentary correction

--- src/main/java/org/openengine/mpn/Match.java
+++ src/main/java/org/openengine/mpn/Match.java
@@ -87,7 +87,7 @@ public class Match {
                     commentary.append(teamName);
                     if (initialPitchPosition != GK) {
                         commentary.append(" under pressure");
-                        commentary.append(" in pitch position ");
+                        commentary.append(" at pitch position ");
                         commentary.append(initialPitchPosition);
                         attachPitchPositionDescription(initialPitchPosition, commentary);
                     } else {
