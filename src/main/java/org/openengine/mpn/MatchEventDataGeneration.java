5230a79c3aae2c001cd72d9d3583082fbcbb04e2
atas <andreas.tasoulas@gmail.com>
2024-05-31 19:30:07 +0300

    Implement kick-off coin toss in event data generation logic

--- src/main/java/org/openengine/mpn/MatchEventDataGeneration.java
+++ src/main/java/org/openengine/mpn/MatchEventDataGeneration.java
@@ -4,13 +4,20 @@ import org.mpn.Dataset;
 import org.mpn.MatchModel;
 import org.mpn.Processable;
 
+import java.util.Random;
+
 public class MatchEventDataGeneration {
 
     private Dataset seedMatchEvents;
     private Dataset generatedMatchEvents;
 
+    private Random rnd = new Random();
+
     private Team homeTeam;
     private Team awayTeam;
+
+    private Team initialKickOffTeam;
+    private Team possessionTeam;
 
     public MatchEventDataGeneration() {
         this.seedMatchEvents = Processable.loadData();
@@ -19,8 +26,26 @@ public class MatchEventDataGeneration {
         awayTeam = new Team("Tottenham", model.getAwayTeamActionChainMappings());
     }
 
+    public static void main(String[] args) {
+        MatchEventDataGeneration match = new MatchEventDataGeneration();
+        match.generate();
+        match.out();
+    }
+
+    public void out() {
+        System.out.println(possessionTeam);
+    }
+
     public void generate() {
         // TODO implement generating of match events
         // refactoring of the start() method of MatchEngine class
+
+        tossCoin();
+    }
+
+    private void tossCoin() {
+        boolean homeTeamKicksOff = rnd.nextBoolean();
+        initialKickOffTeam = homeTeamKicksOff ? homeTeam : awayTeam;
+        possessionTeam = initialKickOffTeam;
     }
 }
